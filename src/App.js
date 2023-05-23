d3da2c356e359d9746ca442f5907791bef5c5db8
Nav Dhamu <n4vdeep@gmail.com>
2023-05-23 18:51:23 +0100

    using keys in components to map objects uniquely

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ class App extends Component {
     this.state = {
       monster: [
         {
+          id: 1,
           name: "Linda",
         },
         {
+          id: 2,
           name: "Frank",
         },
         {
+          id: 3,
           name: "Jacky",
         },
         {
+          id: 4,
           name: "Jessica",
         },
       ],
@@ -27,7 +31,11 @@ class App extends Component {
     return (
       <div className='App'>
         {this.state.monster.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
