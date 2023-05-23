33eb24255112ea5dc6ace0368e18f540f270a8c5
Nav Dhamu <n4vdeep@gmail.com>
2023-05-23 15:46:38 +0100

    mapping arrays to elements withj map()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,40 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {
-        firstName: "Sarah",
-        lastName: "O'Connor",
-      },
-      company: "MethodSVR",
+      monster: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Jessica",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Hi I'm {this.state.name.firstName} {this.state.name.lastName}, and I
-            work at {this.state.company}.
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "Nav", lastName: "Dhamu" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monster.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
