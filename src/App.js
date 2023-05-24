ba37541ae85b4113a1c7e7fd033357763a8f0ee7
Nav Dhamu <n4vdeep@gmail.com>
2023-05-24 08:54:52 +0100

    adding input search box component and working with onChange

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => {
@@ -23,8 +26,14 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
+
     return (
       <div className='App'>
+        <input className='search-box' type='search' placeholder='search monsters...' onChange={(event) => {
+            console.log(event);
+            this.state.monsters.filter(el => el === event.target.value);
+        }}/>
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
