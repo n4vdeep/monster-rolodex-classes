c0d44eb8ea72f661b4168ec4a8bc83dc095b57cf
Nav Dhamu <n4vdeep@gmail.com>
2023-05-26 16:03:20 +0100

    separated out card list and car component and final styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,20 @@
 import { Component } from 'react';
+import '../card-component/card-container.component';
+import './card-list.styles.css';
+import CardComponent from '../card-component/card-container.component';
 
 class CardList extends Component {
   render() {
-    console.log('render from card-list')
-    
+    console.log('render from card-list');
+
     const { monsters } = this.props;
     
     return (
-      <div>
-        {monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className='card-list'>
+        {monsters.map(monster => {
+          return (
+            <CardComponent monster={monster}/>
+        )})};
       </div>
     );
   }
